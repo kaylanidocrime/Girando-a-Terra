1a4a9f51fc946af0c3f2f2484473794b7476d047
kaylani <106694090+kaylanidocrime@users.noreply.github.com>
2024-11-06 15:27:05 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function draw() {
   background(200, 200,200); //cor de fundo
   rotateY(millis()/1000); //rotação da terra
   texture(terra); //textura da terra, imgem.
-  sphere(180, 180); //esfra 3D
+  sphere(190, 190); //esfra 3D
   
   
   }
